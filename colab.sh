d5edcf79f81ce2bf4259989685142188897db696
Carlos Eduardo <stellviaanime@gmail.com>
2023-07-06 15:05:01 -0400

    Agregar go mod tidy a colab.sh

--- colab.sh
+++ colab.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+go mod tidy
 GOOS=linux GOARCH=amd64 go build -o bilateral
 echo builded succesfully
 ./bilateral -input ./input.tif -output ./output.tif -population 10000 -generation 100 -log ./log.txt -parallel 1000
